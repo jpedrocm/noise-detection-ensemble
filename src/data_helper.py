15c093e7f6c1fb35501d49e29fdc304854d5ef67
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-18 13:48:11 -0300

    fix(data_helper.py): fixes extraction of labels

--- src/data_helper.py
+++ src/data_helper.py
@@ -9,32 +9,32 @@ from sklearn.model_selection import StratifiedShuffleSplit
 class DataHelper():
 
 	@staticmethod
-	def extract_feature_labels(dataframe, target_col):
-		print "Extracting features and labels"
+	def extract_feature_labels(frame, target):
+		print("Extracting features and labels")
 
-		labels = dataframe[target_col]
-		feats = dataframe.drop(columns=target_col)
+		labels = frame[target] if target!=-1 else frame.iloc[:, target]
+		feats = frame.drop(columns=labels.name)
 
-		print "Columns: " + str(len(feats.columns))
-		print "Rows: " + str(len(labels))
+		print("Columns: " + str(len(feats.columns)))
+		print("Rows: " + str(len(labels)))
 		return feats, labels
 
 	@staticmethod
-	def split_in_sets(dataframe, labels):
+	def split_in_sets(frame, labels):
 
 		skf = StratifiedKFold(n_splits=3, shuffle=True)
 		return skf.split(X=range(len(labels)), y=labels)[0]
 
 	@staticmethod
-	def select_rows(dataframe, idxs, copy):
-		print "Selecting rows of data"
+	def select_rows(frame, idxs, copy):
+		print("Selecting rows of data")
 
-		sel = dataframe.iloc[idxs]
+		sel = frame.iloc[idxs]
 		sel = deepcopy(sel) if copy == True else sel
 
-		print "Rows: " + str(len(sel))
+		print("Rows: " + str(len(sel)))
 		return sel
 
 	@staticmethod
-	def insert_noise(dataframe, level):
+	def insert_noise(frame, level):
 		pass
