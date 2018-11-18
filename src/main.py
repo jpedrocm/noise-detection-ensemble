475427b1f806b42a9ffa0073ab03e3d97faa7a07
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-18 01:15:02 -0300

    fix(main.py): fix noise insertion and data split

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from io_helper import IOHelper
 
 def main():
 
-	for set_name in ConfigHelper.datasets:
+	for set_name, target_col in ConfigHelper.get_datasets():
 		print("Dataset: " + set_name)
 
 		data = IOHelper.read_dataset(set_name)
@@ -16,15 +16,20 @@ def main():
 		for e in range(ConfigHelper.nb_executions):
 			print("Execution: " + str(e))
 
-			train_data, test_data = DataHelper.split_in_sets(data)
-			train_X, train_y = DataHelper.extract_labels(train_data)
-			test_X, test_y = DataHelper.extract_labels(test_data)
+			feats, labels = DataHelper.extract_feature_labels(data,
+															  target_col)
+
+			train_idxs, test_idxs = DataHelper.split_in_sets(feats, labels)
+
+			train_X = DataHelper.select_rows(feats, train_idxs, copy=False)
+			train_y = DataHelper.select_rows(labels, train_idxs, copy=True)
+			test_X = DataHelper.select_rows(feats, test_idxs, copy=False)
+			test_y = DataHelper.select_rows(labels, test_idxs, copy=True)
 
 			for noise_level in ConfigHelper.noise_levels:
 				print("Noise level: " + str(noise_level))
 
 				noisy_train_y = DataHelper.insert_noise(train_y, noise_level)
-				noisy_test_y = DataHelper.insert_noise(test_y, noise_level)
 
 				for name, clf, clean_type in ConfigHelper.get_classifiers():
 					print("Ensemble: " + name)
