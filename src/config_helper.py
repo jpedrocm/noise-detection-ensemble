1ef2fa30a1d3ac20495e6ce5ee4c482beac8f1d0
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-19 16:24:26 -0300

    fix(config_helper.py): updates classifier calls

--- src/config_helper.py
+++ src/config_helper.py
@@ -14,26 +14,26 @@ class ConfigHelper():
 
 	@staticmethod
 	def get_datasets():
-		return	[("blood",		 None,		-1,  	0,	","), 
-				 ("breast",		 	0,		-1,  None,	","),
-				 ("chess",  	 None,		-1,  None,	","), 
-				 ("german", 	 None, 		-1,	 None,	" "),
-				 ("heart",  	 None, 		-1,	 None,	" "), 
-				 ("ionosphere",  None, 		-1,	 None,	","),
-				 ("parkinsons",  None, "status",	0,	","), 
-				 ("spambase", 	 None, 		-1,	 None,	","), 
-				 ("tic-tac-toe", None, 		-1,	 None,	",")
+		space = " "
+		comma = ","
+		
+		return	[("blood",		 None,		-1,  	0,	comma), 
+				 ("breast",		 	0,		-1,  None,	comma),
+				 ("chess",  	 None,		-1,  None,	comma), 
+				 ("german", 	 None, 		-1,	 None,	space),
+				 ("heart",  	 None, 		-1,	 None,	space), 
+				 ("ionosphere",  None, 		-1,	 None,	comma),
+				 ("parkinsons",  None, "status",	0,	comma), 
+				 ("spambase", 	 None, 		-1,	 None,	comma), 
+				 ("tic-tac-toe", None, 		-1,	 None,	comma)
 				]
 
 	@staticmethod
 	def get_classifiers():
 		return 	[
-				("Fl_rf", RF(n_estimators=501, oob_score=True, n_jobs=-1), 
-							 "fl"),
-				("Cl_rf", RF(n_estimators=501, oob_score=True, n_jobs=-1), 
-							 "cl"),
-				("Fl_maj_rf", RF(n_estimators=501, oob_score=True, n_jobs=-1),
-							 "maj"),
+				("FL_RF", Tree(), "fl"),
+				("CL_RF", Tree(), "cl"),
+				("FL_MAJ_RF", Tree(), "maj"),
 				("RF", RF(n_estimators=500, n_jobs=-1), None),
 				("Boosting", Adaboost(base_estimator=Tree(max_depth=30, 
 									  min_samples_split=20, 
