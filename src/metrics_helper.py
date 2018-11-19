d8dd8cd6523cf1bf581ddfe9bf3af7058660ba70
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-19 19:34:13 -0300

    feat(metrics_helper.py): adds list of metrics attribute to helper

--- src/metrics_helper.py
+++ src/metrics_helper.py
@@ -6,6 +6,8 @@ from sklearn.metrics import accuracy_score
 
 class MetricsHelper():
 
+	metrics = []
+
 	@staticmethod
 	def calculate_error_score(true_y, pred_y):
 		return (1-accuracy_score(true_y, pred_y))
