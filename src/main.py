bb58e041278b2bbb55c8d268dd78ec54045a8d2c
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-20 21:31:34 -0300

    refactor(main.py): uses choose_algorithm function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from config_helper import ConfigHelper
 from io_helper import IOHelper
 from data_helper import DataHelper
 from metrics_helper import MetricsHelper
-from noise_detection_ensemble import NoiseDetectionEnsemble
 
 
 
@@ -41,34 +40,30 @@ def main():
 				for name, clf, clean_type in ConfigHelper.get_classifiers():
 					print("Ensemble: " + name)
 
-					arr_data = None
+					algorithm_data = ConfigHelper.choose_algorithm(clf, 
+																clean_type,
+																train_X,
+																noisy_train_y,
+																max_nb_feats)
 
-					if clean_type == None:
-						arr_data = [None, None, train_X, train_y, clf]
-					elif clean_type == "maj":
-						filt_X, filt_y = MajorityFiltering.run(train_X, 
-															   train_y)
-						arr_data = [None, None, filt_X, filt_y, clf]
-					else:
-						arr_data = NoiseDetectionEnsemble.run(clf, clean_type,
-													   		train_X,
-											   		   		noisy_train_y, 
-											   		   		max_nb_feats)
-						arr_data.append(NoiseDetectionEnsemble.get_ensemble(clf))
+					chosen_rate = algorithm_data[0]
+					chosen_threshold = algorithm_data[1]
+					chosen_X = algorithm_data[2]
+					chosen_y = algorithm_data[3]
+					chosen_clf = algorithm_data[4]
 
-					chosen_rate = arr_data[0]
-					chosen_threshold = arr_data[1]
-					chosen_X = arr_data[2]
-					chosen_y = arr_data[3]
-					chosen_clf = arr_data[4]
 					chosen_clf.fit(chosen_X, chosen_y)
-
 					predictions = chosen_clf.predict(test_X)
 					error = MetricsHelper.calculate_error_score(test_y, 
 															predictions)
+
 					MetricsHelper.metrics.append([set_name, e, noise_level, 
 												name, chosen_rate, 
 												chosen_threshold, error])
+
+			IOHelper.store_results(MetricsHelper.convert_metrics_to_frame(), 
+					set_name)
+			sys.exit()
 
 
 if __name__ == "__main__":
