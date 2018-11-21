32a798a8ac559563249a77661bb094abaf1a6745
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-20 21:31:11 -0300

    feat(config_helper.py): adds algorithm chooser helper function

--- src/config_helper.py
+++ src/config_helper.py
@@ -1,8 +1,13 @@
 ###############################################################################
 
+from numpy import nan
+
 from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.ensemble import AdaBoostClassifier as Adaboost
 from sklearn.tree import DecisionTreeClassifier as Tree
+
+from noise_detection_ensemble import NoiseDetectionEnsemble
+from majority_filtering import MajorityFiltering
 
 
 
@@ -34,11 +39,48 @@ class ConfigHelper():
 		return 	[
 				("FL_RF", Tree(), "fl"),
 				("CL_RF", Tree(), "cl"),
-				("FL_MAJ_RF", RF(n_estimators=501, n_jobs=-1), "maj"),
-				("RF", RF(n_estimators=500, n_jobs=-1), None),
+				#("FL_MAJ_RF", MajorityFiltering.get_ensemble(), "maj"),
+				("RF", RF(n_estimators=501, n_jobs=-1), None),
 				("Boosting", Adaboost(base_estimator=Tree(max_depth=30, 
 									  min_samples_split=20, 
 									  min_impurity_decrease=0.01), 
 									  n_estimators=100, algorithm="SAMME"),
 									  None)
 				]
+
+
+	@staticmethod
+	def choose_algorithm(clf, clean_type, train_X, noisy_train_y, 
+						max_nb_feats):
+		chosen_rate = nan
+		chosen_threshold = nan
+		chosen_X = None
+		chosen_y = None
+		chosen_clf = None
+
+		if clean_type == None:
+			chosen_X = train_X
+			chosen_y = noisy_train_y
+			chosen_clf = clf
+
+		elif clean_type == "maj":
+			filt_X, filt_y = MajorityFiltering.run(clf, 
+												   train_X, 
+												   noisy_train_y)
+			chosen_X = filt_X
+			chosen_y = filt_y
+			chosen_clf = MajorityFiltering.get_ensemble()
+		else:
+			algorithm_data = NoiseDetectionEnsemble.run(clf, clean_type,
+										   		train_X,
+								   		   		noisy_train_y, 
+								   		   		max_nb_feats)
+			chosen_rate = algorithm_data[0]
+			chosen_threshold = algorithm_data[1]
+			chosen_X = algorithm_data[2]
+			chosen_y = algorithm_data[3]
+			chosen_clf = NoiseDetectionEnsemble.get_ensemble(clf, False,
+															chosen_rate,
+															max_nb_feats)
+
+		return [chosen_rate, chosen_threshold, chosen_X, chosen_y, chosen_clf]
