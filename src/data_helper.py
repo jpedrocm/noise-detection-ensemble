b637be99202cd050ab5959c64c92bf732c9a9c2b
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-20 21:30:20 -0300

    fix(data_helper.py): remove prints

--- src/data_helper.py
+++ src/data_helper.py
@@ -14,15 +14,12 @@ class DataHelper():
 
 	@staticmethod
 	def extract_feature_labels(frame, target):
-		print("Extracting features and labels")
 
 		labels = frame[target] if target!=-1 else frame.iloc[:, target]
 		labels = labels.astype("category")
 		feats = frame.drop(columns=labels.name)
 		feats.columns=range(len(feats.columns))
 
-		print("Columns: " + str(len(feats.columns)))
-		print("Rows: " + str(len(labels)))
 		return feats, labels
 
 	@staticmethod
@@ -34,22 +31,21 @@ class DataHelper():
 
 	@staticmethod
 	def select_rows(frame, idxs, copy):
-		print("Selecting rows of data")
 
 		sel = frame.iloc[idxs]
 		sel = deepcopy(sel) if copy == True else sel
-
-		print("Rows: " + str(len(sel)))
 		return sel
 
 	@staticmethod
 	def create_label_mapping(labels):
+
 		unique_values = labels.unique().tolist()
 		DataHelper.label_mapping = {unique_values[0]: unique_values[1],
 						  			unique_values[1]: unique_values[0]}
 
 	@staticmethod
 	def map_labels(labels, sample_idxs, sample_values):
+
 		noise_values = [DataHelper.label_mapping[v] for v in sample_values]
 		noisy_labels = deepcopy(labels)
 		noisy_labels.loc[sample_idxs] = noise_values
@@ -57,10 +53,10 @@ class DataHelper():
 
 	@staticmethod
 	def insert_noise(labels, level):
+
 		sample = labels.sample(frac=level)
 		sample_idxs = sample.index
 		sample_values = sample.values
-
 		return DataHelper.map_labels(labels, sample_idxs, sample_values)
 
 	@staticmethod
