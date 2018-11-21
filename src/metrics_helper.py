783dd8cc7dfd4fbafb8ea328202754cf6e410b8b
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-20 21:29:06 -0300

    feat(metrics_helper.py): adds conversion of metrics to dataframe

--- src/metrics_helper.py
+++ src/metrics_helper.py
@@ -2,6 +2,8 @@
 
 from sklearn.metrics import accuracy_score
 
+from pandas import DataFrame
+
 
 
 class MetricsHelper():
@@ -12,3 +14,9 @@ class MetricsHelper():
 	@staticmethod
 	def calculate_error_score(true_y, pred_y):
 		return (1-accuracy_score(true_y, pred_y))
+
+	@staticmethod
+	def convert_metrics_to_frame():
+		columns = ["dataset", "execution", "noise", "clf", "sampling_rate",
+				   "threshold", "test_error"]
+		return DataFrame(MetricsHelper.metrics, columns=columns)
