68a7dadb78b9c836ed035f592272c93e7f5ff410
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-21 23:11:53 -0300

    feat(metrics_helper.py): adds true filtered nb calculation

--- src/metrics_helper.py
+++ src/metrics_helper.py
@@ -16,11 +16,17 @@ class MetricsHelper():
 		return (1-accuracy_score(true_y, pred_y))
 
 	@staticmethod
+	def calculate_true_filter(y_idxs, noisy_idxs):
+		set_idxs = set(list(y_idxs))
+		true_filtered = [i for i in list(noisy_idxs) if i not in set_idxs]
+		return len(true_filtered)
+
+	@staticmethod
 	def reset_metrics():
 		MetricsHelper.metrics = []
 
 	@staticmethod
 	def convert_metrics_to_frame():
 		columns = ["dataset", "execution", "noise", "clf", "sampling_rate",
-				   "threshold", "test_error"]
+				   "threshold", "test_error", "all_filtered", "true_filtered"]
 		return DataFrame(MetricsHelper.metrics, columns=columns)
