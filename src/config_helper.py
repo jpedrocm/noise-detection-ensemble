c619498d246bdc48e834f4eec15707bb352d6c40
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-22 10:43:11 -0300

    fix(config_helper.py): uncomment model

--- src/config_helper.py
+++ src/config_helper.py
@@ -41,7 +41,7 @@ class ConfigHelper():
 	@staticmethod
 	def get_classifiers():
 		return 	[
-				#("FL_RF", Tree(max_depth=None, min_samples_leaf=1), "fl"),
+				("FL_RF", Tree(max_depth=None, min_samples_leaf=1), "fl"),
 				("CL_RF", Tree(max_depth=None, min_samples_leaf=1), "cl"),
 				("FL_MAJ_RF", MajorityFiltering.get_ensemble(), "maj"),
 				("RF", RF(n_estimators=501, max_depth=None, 
